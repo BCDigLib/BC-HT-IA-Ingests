49d3e61e42f0ce9dd0d7936245de5217ac44ad25
Brian D Meuse <meuseb@bc.edu>
2016-01-22 12:44:04 -0500

    Initial commit. Script to download and stage IA files

---
+++ getIAFiles.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -w
+use strict;
+use FileHandle;
+use LWP::Simple;
+use Switch;
+
+main();
+
+#-----------------------------------------------------------------------------
+sub main
+{
+    
+    showUsage()
+	if scalar(@ARGV) != 2;
+        
+    my ($ids, $type) = @ARGV;
+    
+    my $inputFH = new FileHandle;    
+    $inputFH->open($ids);
+    
+    while(not($inputFH->eof)) {
+        my $id = $inputFH->getline;
+        chomp $id;
+	
+	my $ext;
+	switch ($type) {
+		case 'abbyy' { $ext = '_abbyy.gz' }
+		case 'pdf' { $ext = '.pdf' }
+		case 'orig' { $ext = '_orig_jp2.tar' }
+		case 'jp2' { $ext = '_jp2.zip' }
+	}
+	
+	my $url = 'https://archive.org/download/' . $id . '/' . $id . $ext;
+        my $file = $id . $ext;
+	
+	my $status = getstore($url, $file);
+	
+	if (is_error($status)) {
+		print "ERROR $status while fetching $url\n";
+	}
+    }
+}
+
+
+
+#-----------------------------------------------------------------------------
+sub showUsage
+{
+    print "\nUsage:\n\n";
+    print "getZipFiles.pl ids filetype\n\n";
+    print "Where\nids is file containing IA ids\n\nfiletype can be\nABBYY Full text -> abbyy\nSingle PDF -> pdf\nSingle Page Original JP2 -> orig\nSingle Page Cropped JP2 -> jp2\n";
+    
+    exit 1;
+}
